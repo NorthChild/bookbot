8096362311eeaa95e2a204ed2670a1b27768f6d1
NorthChild <michael.tech933@outlook.com>
2024-08-01 17:11:10 +0100

    fixed bug

--- code/main.py
+++ code/main.py
@@ -15,17 +15,12 @@ def main():
     book_text = utilities.get_book.get_book_text(book_file_path)
     # print(book_text)
     
-    word_count = counters.word_count.book_word_count(book_text)
-    # print("Found a total of: " + str(word_count) + f" words for {book_file_path}")
-    
-    # word_instance_count = counters.word_instance_count.count_instances_of_word(book_text)
-    # # print("Specific words count: " +str(word_count) + f" for {book_file_path}")
-    
-    # specific_word_count = counters.count_specific_word.find_specific_word_count(word_count, input())
-    # # print(specific_word_count)
-    
+    word_count = counters.word_count.book_word_count(book_text)    
     letter_count = counters.letter_instance_count.count_instances_of_letters(book_text)
-    # print("Letters count: " + str(letter_count) + f" for {book_file_path}")
+    word_instance_count = counters.word_instance_count.count_instances_of_word(book_text)
+    
+    # specific_word_count = counters.count_specific_word.find_specific_word_count(word_instance_count, input())
+    # print(specific_word_count)
     
     # print report
     report_builder.report_builder_basic.print_book_report(word_count, letter_count)
